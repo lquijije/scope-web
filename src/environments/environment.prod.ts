04490fd73540dfaa893b34f8ea86ae091d68d97d
Luis Quijije P <lquijije@outlook.com>
2018-12-07 08:44:28 -0500

    adding register page and firebase components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCZ9n25go1PsfCQwObE34E9wXVOaZ1yZds',
+    authDomain: 'scope-app-17b2f.firebaseapp.com',
+    databaseURL: 'https://scope-app-17b2f.firebaseio.com',
+    projectId: 'scope-app-17b2f',
+    storageBucket: 'scope-app-17b2f.appspot.com',
+    messagingSenderId: '913080463170'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCZ9n25go1PsfCQwObE34E9wXVOaZ1yZds',
+    authDomain: 'scope-app-17b2f.firebaseapp.com',
+    databaseURL: 'https://scope-app-17b2f.firebaseio.com',
+    projectId: 'scope-app-17b2f',
+    storageBucket: 'scope-app-17b2f.appspot.com',
+    messagingSenderId: '913080463170'
+  }
 };
 
 /*
