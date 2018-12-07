f28b32cbc1d8d2cd16e05259494c18d73a1e41d8
Luis Quijije P <lquijije@outlook.com>
2018-12-07 14:44:49 -0500

    first test deploy to firebase hosting

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  public cedula: string;
+  public nombres: string;
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    public authServ: AuthService,
+    public router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmitRegisterAddUser() {
+    this.authServ.registerUser(
+      this.email
+      , this.password).then( (res) => {
+        const user = this.authServ.afAuth.auth.currentUser;
+        user.updateProfile({
+          displayName: this.nombres,
+          photoURL: ''
+        }).then(function () {
+          console.log(user);
+        }, function (error) {
+          // An error happened.
+        });
+        // console.log(res);
+        this.router.navigate(['/']);
+      }).catch( (err) => {
+        console.log(err);
+      });
+  }
 }
